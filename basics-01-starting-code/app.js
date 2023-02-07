bc45ffc2cfe2390664d2edc16179a02242360a5c
Shadrack Mwai <47784357+osewemwai@users.noreply.github.com>
2023-02-07 17:15:50 +0300

    output raw

--- basics-01-starting-code/app.js
+++ basics-01-starting-code/app.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
     data: function() {
         return {
-            courseGoal: 'Finish the course and learn Vue!',
+            courseGoalA: `Finish the course and some more!`,
+            courseGoalB: `Master Vue and build amazing apps!`,
             vuelink: 'https://vuejs.org/'
         };
+    },
+    methods: {
+        outputGoal() {
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return this.courseGoalA;
+            } else {
+                return this.courseGoalB;
+            }
+        }
     } // or data() {}
 });
 
